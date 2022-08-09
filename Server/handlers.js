3933a2f7c7123e51e8c2ce56f0ccff366a5cc5fd
Ryan Bajzath <R.bajzath@hotmail.com>
2022-08-09 19:44:23 -0400

    added a createProfile handle

--- Server/handlers.js
+++ Server/handlers.js
@@ -12,18 +12,43 @@ const options = {
 
 //For profiles
 //Create a profile and give it a _id in the body
-const getProfileById = (req, res) => {};
-//Read profile by its :_id param
-const createProfile = (req, res) => {};
+const createProfile = async (req, res) => {
+  const client = new MongoClient(MONGO_URI, options);
+  try {
+    await client.connect();
+    const db = client.db("finalproject");
+    const result = await db.collection("profiles").insertOne(req.body);
+    res.status(201).json({ status: 201, data: result });
+  } catch (err) {
+    console.log(err.stack);
+    res.status(500).json({ status: 500, message: err.message });
+  }
+};
+//Read profile by its :_id param getProfileById
+const getProfile = (req, res) => {};
 //Patch a profile by its :_id param
 const updateProfile = (req, res) => {};
 //Delete profile by its :_id param
 const deleteProfile = (req, res) => {};
 
+//For Appointments
+//Create a getAppointmen and give it a _id in the body
+const getAppointment = (req, res) => {};
+//Read getAppointmen by its :_id param
+const createAppointment = (req, res) => {};
+//Patch a getAppointmen by its :_id param
+const updateAppointment = (req, res) => {};
+//Delete getAppointmen by its :_id param
+const deleteAppointment = (req, res) => {};
+
 //export handlers
 module.exports = {
-  getProfileById,
+  getProfile,
   createProfile,
-  deleteProfile,
   updateProfile,
+  deleteProfile,
+  getAppointment,
+  createAppointment,
+  updateAppointment,
+  deleteAppointment,
 };
--- Server/server.js
+++ Server/server.js
@@ -3,21 +3,31 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
-//import from handlers (destructure)
+//import logic from handlers (destructure)
 const {
-  getProfileById,
+  getProfile,
   createProfile,
   deleteProfile,
   updateProfile,
+  getAppointment,
+  createAppointment,
+  updateAppointment,
+  deleteAppointment,
 } = require(`./handlers`);
 
 express();
 
 //endpoints CRUD for profiles
 app.post(`/profiles`, createProfile);
-app.get(`/profiles/:_id`, getProfileById);
+app.get(`/profiles/:_id`, getProfile);
 app.patch("/profiles/:_id", updateProfile);
 app.delete("/profiles/:_id", deleteProfile);
+
+//endpoints CRUD for Appointments
+app.post(`/Appointments`, getAppointment);
+app.get(`/Appointments/:_id`, createAppointment);
+app.patch("/Appointments/:_id", updateAppointment);
+app.delete("/Appointments/:_id", deleteAppointment);
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
