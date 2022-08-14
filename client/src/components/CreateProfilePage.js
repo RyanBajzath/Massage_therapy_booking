b824d11788ba33a2acdddc5a1552d684ade191f5
Ryan Bajzath <R.bajzath@hotmail.com>
2022-08-14 15:34:49 -0400

    Profile Page style

--- client/src/components/CreateProfilePage.js
+++ client/src/components/CreateProfilePage.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { withAuthenticationRequired } from "@auth0/auth0-react";
 import { NavLink } from "react-router-dom";
+import styled from "styled-components";
 
 const CreateProfilePage = () => {
   const { user } = useAuth0();
@@ -159,226 +160,294 @@ const CreateProfilePage = () => {
   };
 
   return (
-    <div>
+    <CreateProfileContainer>
       CreateProfile
       <form onSubmit={handleSubmit}>
-        <h2>Basic Info</h2>
-        <div>
-          <span>Name:</span>
-          <input
-            onChange={handleUserChange}
-            placeholder="Name here"
-            name="name"
-            type="text"
-            required
-          />
-          <span>Phone number:</span>
-          <input
-            onChange={handleUserChange}
-            placeholder="Phone number here"
-            name="phone"
-            type="text"
-            required
-          />
-          <span>Date of birth:</span>
-          <input
-            onChange={handleUserChange}
-            placeholder="birthday here"
-            name="birthday"
-            type="text"
-            required
-          />
-          <span>Occupation:</span>
-          <input
-            onChange={handleUserChange}
-            placeholder="Occupation here"
-            name="occupation"
-            type="text"
-            required
-          />
-          <span>Emergency Contact:</span>
-          <input
-            onChange={handleUserChange}
-            placeholder="Emergency Contact here"
-            name="emergencyContact"
-            type="text"
-            required
-          />
-          <span>Address:</span>
-          <input
-            onChange={handleUserChange}
-            placeholder="Address Number"
-            name="address"
-            type="text"
-            required
-          />
-
-          <span>Medication</span>
-          <input
-            onChange={handleUserChange}
-            placeholder="Medication"
-            name="medication"
-            type="text"
-            required
-          />
-
-          <span>Any other physical activity?:</span>
-          <input
-            onChange={handleUserChange}
-            placeholder="other physical activity"
-            name="otherPhysicalActivity"
-            type="text"
-            required
-          />
-        </div>
-        <div>
-          <h2>Where do you have tension?:</h2>
-          <span>Head</span>
-          <input
-            type="checkbox"
-            name="head"
-            value="tension"
-            onChange={handleUserChange}
-          />
-          <span>Neck</span>
-          <input
-            type="checkbox"
-            name="neck"
-            value="tension"
-            onChange={handleUserChange}
-          />
-          <span>Left shoulder</span>
-          <input
-            type="checkbox"
-            name="leftShoulder"
-            value="tension"
-            onChange={handleUserChange}
-          />
-          <span>Right shoulder</span>
-          <input
-            type="checkbox"
-            name="RightShoulder"
-            value="tension"
-            onChange={handleUserChange}
-          />
-          <span>Left arm</span>
-          <input
-            type="checkbox"
-            name="leftArm"
-            value="tension"
-            onChange={handleUserChange}
-          />
-          <span>Right arm</span>
-          <input
-            type="checkbox"
-            name="rightArm"
-            value="tension"
-            onChange={handleUserChange}
-          />
-          <span>Upperback</span>
-          <input
-            type="checkbox"
-            name="upperback"
-            value="tension"
-            onChange={handleUserChange}
-          />
-          <span>Lower Back</span>
-          <input
-            type="checkbox"
-            name="loweback"
-            value="tension"
-            onChange={handleUserChange}
-          />
-          <span>Left Leg</span>
-          <input
-            type="checkbox"
-            name="leftLeg"
-            value="tension"
-            onChange={handleUserChange}
-          />
-          <span>Right Leg</span>
-          <input
-            type="checkbox"
-            name="rightLeg"
-            value="tension"
-            onChange={handleUserChange}
-          />
-        </div>
-        <div>
-          <h2>Select those that apply</h2>
-          <span>Circulatory issues</span>
-          <input
-            type="checkbox"
-            name="circulatory"
-            value="tension"
-            onChange={handleUserChange}
-          />
-          <span>Digestive Issues</span>
-          <input
-            type="checkbox"
-            name="digestive"
-            value="tension"
-            onChange={handleUserChange}
-          />
-          <span>Skin Condition</span>
-          <input
-            type="checkbox"
-            name="skin"
-            value="tension"
-            onChange={handleUserChange}
-          />
-          <span>Urinary system issues</span>
-          <input
-            type="checkbox"
-            name="urinary"
-            value="tension"
-            onChange={handleUserChange}
-          />
-          <span>Lymphatic system issues</span>
-          <input
-            type="checkbox"
-            name="lymphatic"
-            value="tension"
-            onChange={handleUserChange}
-          />
-          <span>Nervouse system issues</span>
-          <input
-            type="checkbox"
-            name="nervouse"
-            value="tension"
-            onChange={handleUserChange}
-          />
-          <span>Muscular system isues</span>
-          <input
-            type="checkbox"
-            name="muscular"
-            value="tension"
-            onChange={handleUserChange}
-          />
-          <span>Skeletal system issues</span>
-          <input
-            type="checkbox"
-            name="skeletal"
-            value="tension"
-            onChange={handleUserChange}
-          />
-          <span>Ebdocrine system issues</span>
-          <input
-            type="checkbox"
-            name="ebdocrine"
-            value="tension"
-            onChange={handleUserChange}
-          />
-          <span>Respiratory system issues</span>
-          <input
-            type="checkbox"
-            name="respiratory"
-            value="tension"
-            onChange={handleUserChange}
-          />
-        </div>
-        <div>
-          <h2>Massage Session information</h2>
+        <StyledTitle>Basic Info</StyledTitle>
+        <BasicInfoDiv>
+          <BasicInfoDivTop>
+            {/* <span>Name:</span>
+             */}
+            <div>
+              <label for="name">First name:</label>
+              <input
+                onChange={handleUserChange}
+                placeholder="Name here"
+                name="name"
+                type="text"
+                required
+              />
+            </div>
+            <div>
+              <label for="phone">Phone :</label>
+              <input
+                onChange={handleUserChange}
+                placeholder="Phone number here"
+                name="phone"
+                type="text"
+                required
+              />
+            </div>
+            <div>
+              <label for="birthday">Birthday :</label>
+              <input
+                onChange={handleUserChange}
+                placeholder="birthday here"
+                name="birthday"
+                type="text"
+                required
+              />
+            </div>
+            <div>
+              <label for="occupation">Occupation :</label>
+              <input
+                onChange={handleUserChange}
+                placeholder="Occupation here"
+                name="occupation"
+                type="text"
+                required
+              />
+            </div>
+          </BasicInfoDivTop>
+          <BasicInfoDivBottom>
+            <div>
+              <span>Emergency Contact:</span>
+              <input
+                onChange={handleUserChange}
+                placeholder="Emergency Contact here"
+                name="emergencyContact"
+                type="text"
+                required
+              />
+            </div>
+            <div>
+              <span>Address:</span>
+              <input
+                onChange={handleUserChange}
+                placeholder="Address Number"
+                name="address"
+                type="text"
+                required
+              />
+            </div>
+            <div>
+              <span>Medication</span>
+              <input
+                onChange={handleUserChange}
+                placeholder="Medication"
+                name="medication"
+                type="text"
+                required
+              />
+            </div>
+            <div>
+              <span>Any other physical activity?:</span>
+              <input
+                onChange={handleUserChange}
+                placeholder="other physical activity"
+                name="otherPhysicalActivity"
+                type="text"
+                required
+              />
+            </div>
+          </BasicInfoDivBottom>
+        </BasicInfoDiv>
+        <StyledTitle>Where do you have tension?</StyledTitle>
+        <TensionDiv>
+          <TensionDivTop>
+            <div>
+              <span>Head</span>
+              <input
+                type="checkbox"
+                name="head"
+                value="tension"
+                onChange={handleUserChange}
+              />
+            </div>
+            <div>
+              <span>Neck</span>
+              <input
+                type="checkbox"
+                name="neck"
+                value="tension"
+                onChange={handleUserChange}
+              />
+            </div>
+            <div>
+              <span>Left shoulder</span>
+              <input
+                type="checkbox"
+                name="leftShoulder"
+                value="tension"
+                onChange={handleUserChange}
+              />
+            </div>
+            <div>
+              <span>Right shoulder</span>
+              <input
+                type="checkbox"
+                name="RightShoulder"
+                value="tension"
+                onChange={handleUserChange}
+              />
+            </div>
+            <div>
+              <span>Left arm</span>
+              <input
+                type="checkbox"
+                name="leftArm"
+                value="tension"
+                onChange={handleUserChange}
+              />
+            </div>
+          </TensionDivTop>
+          <TensionDivBottom>
+            <div>
+              <span>Right arm</span>
+              <input
+                type="checkbox"
+                name="rightArm"
+                value="tension"
+                onChange={handleUserChange}
+              />{" "}
+            </div>{" "}
+            <div>
+              <span>Upperback</span>
+              <input
+                type="checkbox"
+                name="upperback"
+                value="tension"
+                onChange={handleUserChange}
+              />{" "}
+            </div>{" "}
+            <div>
+              <span>Lower Back</span>
+              <input
+                type="checkbox"
+                name="loweback"
+                value="tension"
+                onChange={handleUserChange}
+              />{" "}
+            </div>{" "}
+            <div>
+              <span>Left Leg</span>
+              <input
+                type="checkbox"
+                name="leftLeg"
+                value="tension"
+                onChange={handleUserChange}
+              />{" "}
+            </div>{" "}
+            <div>
+              <span>Right Leg</span>
+              <input
+                type="checkbox"
+                name="rightLeg"
+                value="tension"
+                onChange={handleUserChange}
+              />
+            </div>
+          </TensionDivBottom>
+        </TensionDiv>
+        <StyledTitle>Select those that apply</StyledTitle>
+        <HealthIssuesDiv>
+          <HealthIssuesDivTop>
+            <div>
+              <span>Circulatory issues</span>
+              <input
+                type="checkbox"
+                name="circulatory"
+                value="tension"
+                onChange={handleUserChange}
+              />
+            </div>
+            <div>
+              <span>Digestive Issues</span>
+              <input
+                type="checkbox"
+                name="digestive"
+                value="tension"
+                onChange={handleUserChange}
+              />
+            </div>
+            <div>
+              <span>Skin Condition</span>
+              <input
+                type="checkbox"
+                name="skin"
+                value="tension"
+                onChange={handleUserChange}
+              />
+            </div>
+            <div>
+              <span>Urinary system issues</span>
+              <input
+                type="checkbox"
+                name="urinary"
+                value="tension"
+                onChange={handleUserChange}
+              />
+            </div>
+            <div>
+              <span>Lymphatic system issues</span>
+              <input
+                type="checkbox"
+                name="lymphatic"
+                value="tension"
+                onChange={handleUserChange}
+              />
+            </div>
+          </HealthIssuesDivTop>
+          <HealthIssuesDivBottom>
+            <div>
+              <span>Nervouse system issues</span>
+              <input
+                type="checkbox"
+                name="nervouse"
+                value="tension"
+                onChange={handleUserChange}
+              />
+            </div>
+            <div>
+              <span>Muscular system isues</span>
+              <input
+                type="checkbox"
+                name="muscular"
+                value="tension"
+                onChange={handleUserChange}
+              />
+            </div>
+            <div>
+              <span>Skeletal system issues</span>
+              <input
+                type="checkbox"
+                name="skeletal"
+                value="tension"
+                onChange={handleUserChange}
+              />
+            </div>
+            <div>
+              <span>Ebdocrine system issues</span>
+              <input
+                type="checkbox"
+                name="ebdocrine"
+                value="tension"
+                onChange={handleUserChange}
+              />
+            </div>
+            <div>
+              <span>Respiratory system issues</span>
+              <input
+                type="checkbox"
+                name="respiratory"
+                value="tension"
+                onChange={handleUserChange}
+              />
+            </div>
+          </HealthIssuesDivBottom>
+        </HealthIssuesDiv>
+        <StyledTitle>Massage Session information</StyledTitle>
+        <SessioninfoDiv>
           <span>Please list any areas you wish us to avoid:</span>
           <input
             onChange={handleUserChange}
@@ -395,12 +464,102 @@ const CreateProfilePage = () => {
             type="text"
             required
           />
-        </div>
-        <button>Create Profile</button>
+        </SessioninfoDiv>
+        <ButtonDiv>
+          <Button>Create Profile</Button>
+          <PandaMilk src="https://media.giphy.com/media/1PXZZyQwpdfzYkmS3R/giphy.gif" />
+        </ButtonDiv>
       </form>
-    </div>
+    </CreateProfileContainer>
   );
 };
+
+const CreateProfileContainer = styled.div`
+  display: flex;
+  /* align-content: center; */
+  flex-direction: column;
+  background-color: mistyrose;
+  height: 100vh;
+  border-right: black solid 4px;
+  border-left: black solid 4px;
+  border-bottom: black solid 4px;
+`;
+
+const StyledTitle = styled.h2`
+  display: flex;
+  border-bottom: 3px gray solid;
+  margin-left: 2vw;
+`;
+
+const BasicInfoDiv = styled.div`
+  margin-left: 2vw;
+  width: 90vw;
+`;
+
+const BasicInfoDivTop = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const BasicInfoDivBottom = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const TensionDiv = styled.div`
+  margin-left: 2vw;
+  width: 90vw;
+`;
+
+const TensionDivTop = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const TensionDivBottom = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const HealthIssuesDiv = styled.div`
+  margin-left: 2vw;
+  width: 90vw;
+`;
+
+const HealthIssuesDivTop = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const HealthIssuesDivBottom = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const SessioninfoDiv = styled.div`
+  margin-left: 2vw;
+  width: 90vw;
+`;
+const ButtonDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  /* margin-top: 30px; */
+
+  align-items: center;
+`;
+
+const Button = styled.button`
+  /* text-decoration: none; */
+  border-radius: 18px;
+  font-size: 50px;
+  border: 3px black solid;
+  background-color: RGB(149, 125, 173);
+  max-height: 10vh;
+`;
+
+const PandaMilk = styled.img`
+  height: 30vh;
+`;
 
 export default withAuthenticationRequired(CreateProfilePage, {
   // Show a message while the user waits to be redirected to the login page.
