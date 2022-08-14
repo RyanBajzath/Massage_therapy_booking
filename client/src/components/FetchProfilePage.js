c9e29525dc1e6fe503850c25a964ca9e22c2f5b5
Ryan Bajzath <R.bajzath@hotmail.com>
2022-08-14 00:29:45 -0400

    styled fetchProfilePage

--- client/src/components/FetchProfilePage.js
+++ client/src/components/FetchProfilePage.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { withAuthenticationRequired } from "@auth0/auth0-react";
+import styled from "styled-components";
 
 const FetchProfilepage = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
@@ -26,8 +27,9 @@ const FetchProfilepage = () => {
 
   return (
     isAuthenticated && (
-      <div>
-        <div>
+      <ProfileInfoDiv>
+        <h2>Basic Information</h2>
+        <BasicInfoDiv>
           <p>name: {userInfo.name}</p>
           <p>phone: {userInfo.phone}</p>
           <p>birthday: {userInfo.birthday}</p>
@@ -38,36 +40,147 @@ const FetchProfilepage = () => {
           <p>otherPhysicalActivity: {userInfo.otherPhysicalActivity}</p>
           <p>avoidAreas: {userInfo.avoidAreas}</p>
           <p>goals: {userInfo.goals}</p>
-          <p>{userInfo.head === true && "Tension in head"}</p>
-          <p>{userInfo.neck === true && "Tension in neck"}</p>
-          <p>{userInfo.leftShoulder === true && "Tension in leftShoulder"}</p>
-          <p>{userInfo.RightShoulder === true && "Tension in RightShoulder"}</p>
-          <p>{userInfo.leftArm === true && "Tension in leftArm"}</p>
-          <p>{userInfo.rightArm === true && "Tension in rightArm"}</p>
-          <p>{userInfo.upperback === true && "Tension in upperback"}</p>
-          <p>{userInfo.loweback === true && "Tension in loweback"}</p>
-          <p>{userInfo.leftLeg === true && "Tension in leftLeg"}</p>
-          <p>{userInfo.rightLeg === true && "Tension in rightLeg"}</p>
-          <p>
-            {userInfo.circulatory === true && "Issue with circulatory system"}
-          </p>
-          <p>{userInfo.digestive === true && "Issue with digestive system"}</p>
-          <p>{userInfo.skin === true && "Issue with skin"}</p>
-          <p>{userInfo.urinary === true && "Issue with urinary system"}</p>
-          <p>{userInfo.lymphatic === true && "Issue with lymphatic system"}</p>
-          <p>{userInfo.nervouse === true && "Issue with nervouse system"}</p>
-          <p>{userInfo.muscular === true && "Issue with muscular system"}</p>
-          <p>{userInfo.skeletal === true && "Issue with skeletal system"}</p>
-          <p>{userInfo.ebdocrine === true && "Issue with ebdocrine system"}</p>
-          <p>
-            {userInfo.respiratory === true && "Issue with respiratory system"}
-          </p>
-          <p>Next Appoitment: {userInfo.appointment}</p>
+        </BasicInfoDiv>
+        <h2>Tension location</h2>
+        <TensionLocationDiv>
+          <p>{userInfo.head === true && "head"}</p>
+          <p>{userInfo.neck === true && "neck"}</p>
+          <p>{userInfo.leftShoulder === true && "leftShoulder"}</p>
+          <p>{userInfo.RightShoulder === true && "RightShoulder"}</p>
+          <p>{userInfo.leftArm === true && "leftArm"}</p>
+          <p>{userInfo.rightArm === true && "rightArm"}</p>
+          <p>{userInfo.upperback === true && "upperback"}</p>
+          <p>{userInfo.loweback === true && "loweback"}</p>
+          <p>{userInfo.leftLeg === true && "leftLeg"}</p>
+          <p>{userInfo.rightLeg === true && "rightLeg"}</p>
+        </TensionLocationDiv>
+        <h2>Health issues</h2>
+        <HealthIssuesDiv>
+          <p>{userInfo.circulatory === true && "circulatory system"}</p>
+          <p>{userInfo.digestive === true && "digestive system"}</p>
+          <p>{userInfo.skin === true && "skin"}</p>
+          <p>{userInfo.urinary === true && "urinary system"}</p>
+          <p>{userInfo.lymphatic === true && "lymphatic system"}</p>
+          <p>{userInfo.nervouse === true && "nervouse system"}</p>
+          <p>{userInfo.muscular === true && "muscular system"}</p>
+          <p>{userInfo.skeletal === true && "skeletal system"}</p>
+          <p>{userInfo.ebdocrine === true && "ebdocrine system"}</p>
+          <p>{userInfo.respiratory === true && "respiratory system"}</p>
+        </HealthIssuesDiv>
+        <div>
+          <h2>Next appointment</h2>
+          <p>{userInfo.appointment}</p>
         </div>
-      </div>
+        <h2>Recommended stretches</h2>
+
+        <RecommendedDiv>
+          <PandaImg src={require("../images/panda.png")} />
+          <div>
+            {userInfo.neck && (
+              <StretchImg src={require("../images/neckStretch.png")} />
+            )}
+          </div>
+          <div>
+            {userInfo.LeftShoulder && (
+              <StretchImg src={require("../images/shoulderStretch.png")} />
+            )}
+          </div>
+          {/* <div>
+            {userInfo.RightShoulder && (
+              <img src={require("../images/shoulderStretch.png")} />
+            )}
+          </div> */}
+          {/* <div>
+            {userInfo.leftArm && (
+              <img src={require("../images/neckStretch.png")} />
+            )}
+          </div>
+          <div>
+            {userInfo.rightArm && (
+              <img src={require("../images/neckStretch.png")} />
+            )}
+          </div> */}
+          <div>
+            <p>
+              {userInfo.upperback && (
+                <StretchImg src={require("../images/back.png")} />
+              )}
+            </p>
+          </div>
+          <div>
+            <p>
+              {userInfo.loweback && (
+                <StretchImg src={require("../images/lowerBackStretch.png")} />
+              )}
+            </p>
+          </div>
+          <div>
+            <p>
+              {userInfo.leftLeg && (
+                <StretchImg src={require("../images/legStretch.png")} />
+              )}
+            </p>
+          </div>
+          {/* <div>
+            {userInfo.rightLeg && (
+              <img src={require("../images/legStretch.png")} />
+            )}
+          </div> */}
+        </RecommendedDiv>
+      </ProfileInfoDiv>
     )
   );
 };
+
+const ProfileInfoDiv = styled.div`
+  display: flex;
+  align-content: center;
+  flex-direction: column;
+  /* align-items: center; */
+  background-color: mistyrose;
+  height: 100vh;
+  border-right: black solid 4px;
+  border-left: black solid 4px;
+  border-bottom: black solid 4px;
+`;
+
+const BasicInfoDiv = styled.div`
+  display: flex;
+  gap: 20px;
+  /* font-weight: bold; */
+  font-size: 20px;
+  margin-left: 20px;
+`;
+
+const TensionLocationDiv = styled.div`
+  display: flex;
+  gap: 20px;
+  font-size: 20px;
+  margin-left: 20px;
+`;
+
+const HealthIssuesDiv = styled.div`
+  display: flex;
+  gap: 20px;
+  font-size: 20px;
+  margin-left: 20px;
+`;
+
+const PandaImg = styled.img`
+  height: 150px;
+  width: 150px;
+`;
+
+const RecommendedDiv = styled.div`
+  display: flex;
+  justify-content: space-around;
+  margin-left: 20px;
+`;
+const StretchImg = styled.img`
+  /* height: 200px; */
+  width: 300px;
+  /* margin-left: -50px; */
+`;
 
 export default withAuthenticationRequired(FetchProfilepage, {
   // Show a message while the user waits to be redirected to the login page.
